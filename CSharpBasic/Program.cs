ace08d4167ad8a66b7aebbd177ed270e5cc68c11
hktrollgames <hktrollgames@gmail.com>
2021-05-08 15:43:53 +0900

    초기 커밋.

---
+++ CSharpBasic/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace chapter1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("헬로 월드" );
+            // 콘솔 프로그램 실행, 실행파일 위치. 디버그, 릴리즈, 디버깅?
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("입력한 아규먼트" + args[i]);
+            }
+
+            // 자료형
+            {
+                int i = -1; //4byte
+                uint ui = 1; // 4byte unsigned(부호 없는 -> 부호 넣을 수없음 -> 음수 불가)
+                //double d = 1.1; // 8Byte
+                float f = 1.1f; // 4Byte
+            }
+
+            // 자료(리터별)의 기본 타입.
+            // var : 자동으로 유추된 타입.
+            {
+                var 소수점없는숫자 = 1;
+                var 소수점 = 1.1;
+                var 문자열 = "문자열";
+                var 한글자 = '가';
+
+                var 소수점없는숫자D = 1d;
+                var 소수점F = 1.1f;
+            }
+
+
+            // 배열
+            {
+                int i = 1;
+
+                int[] intArray = new int[10];
+                intArray[0] = 1; // 인덱스는 0부터 시작.
+                intArray[1] = 3;
+                intArray[2] = 5;
+                //...
+                intArray[9] = 99;
+                // 메모리 뷰 Ctrl + Alt + M, 1
+                // &intArray[0] 첫번째 요소의 주소
+            }
+
+
+            // 자료형별 크기.
+            {
+                // 1 Byte = 8bit
+                // 2 Byte = 16bit
+                // 4 Byte = 32bit
+                // 8 Byte = 64bit
+
+                short s;// 2Byte    Int16
+                int i;  // 4Byte    Int32
+                long l; // 8Byte    Int64
+                byte by; // 1Byte
+                char c; // 2Byte 
+                bool bo; // 1Byte
+                char[] boArray = new char[2];
+                boArray[0] = 'a';
+                boArray[1] = 'b';
+            }
+
+            // 값 타입과 참조타입
+            {
+                // 값타입.
+                int i = 0;
+                MyStruct myStruct;
+                myStruct.i = 1;
+
+                // 참조타입 -> 주소만 가진다. 주소의 크기 : 32비트 프로그램 -> 4Byte, 64비트 프로그램 8Byte
+                string str;
+                MyClass myClass;
+                myClass = new MyClass();
+                myClass.i = 1;  // 숫자1인 메모리상에서 1
+                str = "1";      // 글자1은 메모리상에서 16진수 31 (아스키 코드표 참고) https://namu.wiki/w/아스키코드
+            }
+
+            // 자료형별 기본값 -> 메모리에 값이 0일때의 값.
+            {
+                int i;
+                bool b;
+                string str1;
+                str1 = "abc";   // 메모리뷰에서 str1에 글자를 할당해서
+                                // &str1, str1값이 어떻게 변하는지 확인
+                                // 스트링은 immutable 타입이라서
+                                // 글자를 할당할때마다 새로운 메모리 주소에 글자를 할당한다
+                //* mutable(변하기 쉬운)은 변경이 가능하고 immutable은 변경이 불가능합니다.
+            }
+        }
+
+        struct MyStruct
+        {
+            public int i;
+            public int x;
+            public char c;
+        }
+        class MyClass
+        {
+            public int i;
+        }
+    }
+}
---
+++ Chapter2/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+
+namespace Chapter2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 문자열.
+            string str = " 자주쓰는 스트링 예제 !!!  ";
+
+            // 문자
+            char c = 'a';//쌍따움표 아님!,  한글만 입력가능.
+            c = str[1]; //자
+
+            {
+                // 문자열 결합
+                string str1 = str + "어렵게 생각말자" + 'c';
+                int i = 1;
+                string str2 = "나의 숫자는 : " + i;
+                string str3 = string.Format("나의{2} 숫자는 {0} 입니다, {1}"
+                    , i, 2, "11", "4번째 파라미터");
+                string str4 = $"나의 숫자는 {i}입니다 {111 + 1}, {true}";
+                string str5 = @"골뱅이는 무슨 의미일까요?
+골뱅이 첫번째줄
+            골뱅이 두번째줄";
+                string str6 = "골뱅이는 무슨 의미일까요?\n" +
+"골뱅이 첫번재줄\n" +
+"           골배이 두번째줄";
+
+                Console.WriteLine(str5);
+                Console.WriteLine(str6);
+            }
+
+            //
+            {
+                string str1 = str.Trim();
+                Console.WriteLine("str1:" + str1);
+                Console.WriteLine("str:" + str);
+            }
+
+            {
+                string str1 = str.Replace("스트링", "글자");
+                string str2 = str.Replace("스", "S");
+                string str3 = str.Replace('스', 'S');
+                string str4 = str.Replace(" ", ""); // 치환을 사용한 삭제
+            }
+
+            // 연산자.
+            // 산술 연산자
+            {
+                ////+ - * / %
+                int i = 3 % 2; // 1
+            }
+
+            // 할당 연산자
+            {
+                int a = 1;
+                a = a + 2; 
+                a += 2; // 같은 의미 a = a + 2; 
+            }
+
+
+            // 논리 연산자 && || !(Not)
+            {
+                bool isTrue = true;
+                if (isTrue)
+                    Console.WriteLine("참일때 실행됨");
+
+                if (true && false)
+                    Console.WriteLine("실행안됨");
+
+                if (true || false)
+                    Console.WriteLine("실행됨");
+
+                if (!true)
+                    Console.WriteLine("실행안됨");
+            }
+
+            // 증감 연산자.
+            {
+                int i = 0;
+                int j = i++; //j : 0
+                int k = ++i;
+
+                if (++i > 0 || j++ > 0)
+                    k = 0;
+
+                // 여기서 j는 무엇을까?
+            }
+
+            // 비교 연산자  >=, <, <=, == 
+            {
+                int i = 1;
+                // > 크다.
+                // < 작다
+                // >= 크거나 같다
+                // <= 작거나 같다
+
+                // == 같다.
+                // != 다르다.
+                if (i >= 100) { }
+            }
+
+            // 조건 연산자.
+            {
+                // ? : -> 삼항 연산자.
+                int val;
+                int x = 0, y = 1;
+                if (x > y)
+                {
+                    val = x;
+                }
+                else
+                {
+                    val = y;
+                }
+
+                val = (x > y) ? x : y;
+                val = x > y ? x : y;  //()생략 가능.
+
+                //// ??
+                string s = null;
+                string ss = s ?? "스트링이 비어 있다";
+            }
+
+
+            오늘의_할일출력(5);
+
+
+
+            //열거형
+            //DayOfWeek 
+            //요일별 할일 프로그램.
+            void 오늘의_할일출력(int dayOfWeek)
+            {
+                switch (dayOfWeek)
+                {
+                    case 0:
+                        Console.WriteLine("학원가는날");
+                        break;
+                    case 1:
+                            Console.WriteLine("화요일도 학원가는날");
+                            Console.WriteLine("화요일도 학원가는날");
+                        break;
+                    case 5:
+                    case 6:
+                        Console.WriteLine("주말은 학생들 자습하는날, 강사는 노는날");
+                        break;
+                    default:
+                        Console.WriteLine("입력한 값은 " + dayOfWeek +"입니다");
+                        break;
+                }
+
+
+            }
+
+
+            DayOfWeek dayOfWeek;
+            dayOfWeek = DayOfWeek.Sunday;
+            오늘의_할일출력_EnumVersion(dayOfWeek);
+
+            void 오늘의_할일출력_EnumVersion(DayOfWeek dayOfWeek)
+            {
+                switch (dayOfWeek)
+                {
+                    case DayOfWeek.Monday: Console.WriteLine("학원가는날"); break;
+                    case DayOfWeek.Tuesday:
+                        {
+                            Console.WriteLine("화요일도 학원가는날");
+                        }
+                        break;
+                    case DayOfWeek.Saturday:
+                    case DayOfWeek.Sunday:
+                        Console.WriteLine("주말은 자습하는날");
+                        break;
+                }
+
+                //Flags 
+                bool 토요일포함되었나 = dayOfWeek.HasFlag(DayOfWeek.Saturday);
+            }
+        }
+
+        //[Flags]
+        enum DayOfWeek
+        {
+            Sunday      ,
+            Monday      ,
+            Tuesday     ,
+            Wednesday   ,
+            Thursday    ,
+            Friday      ,
+            Saturday    
+        }
+    }
+}
